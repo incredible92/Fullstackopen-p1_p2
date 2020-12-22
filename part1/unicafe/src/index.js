c9ce7f4c2557d2b54a28d34ed64eb79e0a62b9a8
incredible92 <sulymanabdullahi@gmail.com>
2020-12-22 16:05:41 +0100

    feat: statistic

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,6 +7,17 @@ const Button = (props) => (
   </button>
 )
 
+const Statistic = ({text, value}) => {
+  return (
+    <table>
+        <tr>
+          <td>{text}</td>
+          <td>{value}</td>
+        </tr>
+    </table>
+  );
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -15,9 +26,18 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text="Good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1>Statistics</h1>
+
+      <Statistic text="good" value ={good} />
+      <Statistic text="neutral" value ={neutral} />
+      <Statistic text="bad" value ={bad} />
+      <Statistic text="All" value={(good + neutral + bad)} />
+      
+  
+      <h3>No feedback given</h3>
   
     </div>
   )
