c048e7942537acf904be1f3bf99b8d4e3d138a46
incredible92 <sulymanabdullahi@gmail.com>
2021-01-09 11:42:30 +0100

    fix: phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,27 @@
+import React, { useState } from 'react'
 
-import './App.css';
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
 
-function App() {
+  
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
