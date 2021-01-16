70d45278813f49f8d9fdedd6d0c4abde26a693b1
incredible92 <sulymanabdullahi@gmail.com>
2021-01-16 11:29:52 +0100

    fix: functionality

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react'
 
+import Notification from './components/notification.jsx'
+import Filter from './components/filter.jsx'
+
 
 const App = () => {
-  
+  const [ persons, setPersons] = useState([])
+  const [errorMessage, setErrorMessage] = useState({
+	  message: null,
+	  category: null
+  })
+  const [showResults, setResults] = useState('')
+  const filterChange = (event) => {
+	  setResults(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <h2>Add name</h2>
+      <Notification m={errorMessage} />
+      <Filter showResults={showResults} filterChange={filterChange} />
+      <h2>Add new</h2>
     <h2>Number</h2>
     </div>
   )
